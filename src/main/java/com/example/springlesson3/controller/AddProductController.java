91c29139463e90a07d3db8105ac1f819a3e1c801
Student <123@12d.dd>
2021-10-17 23:43:18 +0800

    backup

--- src/main/java/com/example/springlesson3/controller/AddProductController.java
+++ src/main/java/com/example/springlesson3/controller/AddProductController.java
@@ -1,6 +1,7 @@
 package com.example.springlesson3.controller;
 
 import com.example.springlesson3.domain.Product;
+import com.example.springlesson3.interfaces.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+
 public class AddProductController {
 
-    @GetMapping("/addProduct")
+    private ProductService productService;
+
+    public AddProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+    @GetMapping("/addProduct") //получаем форму
     public String addForm(Model model) {
         model.addAttribute("addProduct", new Product());
         return "product/addProduct";
@@ -18,7 +27,12 @@ public class AddProductController {
 
     @PostMapping("/addProduct")
     public String addSubmit(@ModelAttribute Product addProduct, Model model) {
-        model.addAttribute("addProduct", addProduct);
+
+
+        System.out.println(addProduct);
+        productService.addProduct(addProduct);
+        System.out.println(productService.getProducts());// изза зашглушки показывает старое
+        model.addAttribute("addProduct");
         return "product/productList";
     }
 
--- src/main/java/com/example/springlesson3/service/ProductServiceImp.java
+++ src/main/java/com/example/springlesson3/service/ProductServiceImp.java
@@ -4,11 +4,13 @@ import com.example.springlesson3.domain.Product;
 import com.example.springlesson3.interfaces.ProductRepository;
 import com.example.springlesson3.interfaces.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+//@ToString
 @RequiredArgsConstructor /*включает в конструтор только необходимые аргументы*/
 public class ProductServiceImp implements ProductService {
 
@@ -17,5 +19,10 @@ public class ProductServiceImp implements ProductService {
     @Override
     public List<Product> getProducts() {
         return productRepository.getAll();
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        productRepository.addNew(product);
     }
 }
